1ec6b81722eec74051e7d1ed133bb5a211ffb7ce
olivrz <oliver.rzep@gmail.com>
2024-01-02 15:43:23 -0500

    update logo

--- components/global-nav.tsx
+++ components/global-nav.tsx
@@ -5,7 +5,7 @@ export default function GlobalNav() {
 
     return (
         <div className="flex flex-row">
-            <Image src={Logo} alt="Company Logo"/>
+            <Image src={Logo} alt="React Gates Logo" className="w-1/8" objectFit="contain" objectPosition="center"/>
             <div>
                 <h1>React Gates</h1>
             </div>
